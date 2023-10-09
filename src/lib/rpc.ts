adf645c896f54b84dc25bf15808fc67a872b1c67
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2023-10-09 14:36:17 -0400

    added rpc timeout

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -2,14 +2,28 @@ import { RPCResponse } from './types'
 
 export class RPC {
   endpoint: string
+  timeout: number
   constructor(endpoint: string) {
     this.endpoint = endpoint
+    this.timeout = 3000
   }
 
   async post<T>(method: string, params?: any): Promise<RPCResponse<T>> {
     try {
+      const controller = new AbortController()
       const body = JSON.stringify({ id: 1, jsonrpc: '2.0', method: method, params })
-      const res = await fetch(this.endpoint, { method: `POST`, body })
+
+      const timeoutId = setTimeout(() => {
+        controller.abort()
+      }, this.timeout)
+
+      const res = await fetch(this.endpoint, {
+        method: `POST`,
+        body,
+        signal: controller.signal
+      })
+
+      clearTimeout(timeoutId)
 
       if (res.ok) {
         const data = await res.json() as RPCResponse<T>
