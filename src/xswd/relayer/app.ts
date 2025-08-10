02c202941421e75da69dc754f7d76dff6deed9b0
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2025-08-10 11:29:33 -0400

    add relayer app style and animation

--- src/xswd/relayer/app.ts
+++ src/xswd/relayer/app.ts
@@ -40,7 +40,21 @@ export class App {
         this.qrCodeElement = document.createElement(`div`)
         this.qrCodeElement.classList.add(`xelis-xswd-relayer-qrcode`)
         this.qrCodeElement.innerHTML = `
-            <div>XSWD Relayer</div>
+            <div class="xelis-xswd-relayer-scan-logo">
+                <svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4,4h6v6H4V4M20,4v6H14V4h6M14,15h2V13H14V11h2v2h2V11h2v2H18v2h2v3H18v2H16V18H13v2H11V16h3V15m2,0v3h2V15H16M4,20V14h6v6H4M6,6V8H8V6H6M16,6V8h2V6H16M6,16v2H8V16H6M4,11H6v2H4V11m5,0h4v4H11V13H9V11m2-5h2v4H11V6M2,2V6H0V2A2,2,0,0,1,2,0H6V2H2M22,0a2,2,0,0,1,2,2V6H22V2H18V0h4M2,18v4H6v2H2a2,2,0,0,1-2-2V18H2m20,4V18h2v4a2,2,0,0,1-2,2H18V22Z"/>
+                </svg>
+            </div>
+            <div class="xelis-xswd-relayer-logo-wrap">
+                <div class="xelis-xswd-relayer-logo">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 778 743" fill="currentColor">
+                        <path fillRule="evenodd" clipRule="evenodd" d="M388.909 742.872L777.817 353.964L424.056 0.202599L478.809 132.737L700.036 353.964L388.909 665.091L77.7817 353.964L299.507 129.121L353.964 0L0 353.964L388.909 742.872Z" />
+                        <path d="M388.909 665.091L353.964 0L299.507 129.121L388.909 665.091Z" />
+                        <path d="M424.056 0.202599L388.909 665.091L478.809 132.737L424.056 0.202599Z" />
+                    </svg>
+                </div>
+            </div>
+            <div class="xelis-xswd-relayer-title">XSWD Relayer</div>
             <canvas></canvas>
         `
 
@@ -63,7 +77,9 @@ export class App {
 
     setError(msg: string) {
         this.clear()
-        this.errElement.innerHTML = `error: ${msg}`
+        this.errElement.innerHTML = `
+            <div class="xelis-xswd-relayer-error-text">error: ${msg}</div>
+        `
         this.contentElement.appendChild(this.errElement)
     }
 
@@ -84,7 +100,12 @@ export class App {
             encryption_mode: this.relayer.props.encryption_mode
         } as RelayerAppData)
 
-        QRCode.toCanvas(canvas, qrCodeData);
+        QRCode.toCanvas(canvas, qrCodeData, { 
+            color: { 
+                dark: `#fff`, // dots
+                light: `#000` // background
+            }
+        })
 
         this.contentElement.appendChild(this.qrCodeElement)
     }
